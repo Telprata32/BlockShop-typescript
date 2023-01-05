2698ad1831c9e3e43c90967679c5ce24cbeae163
Rahim <zero_degree_96@hotmail.com>
2023-01-05 15:16:05 +0800

    Modify App.tsx to accomodate all added frontend pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Login from "./Pages/Login";
+import Registration from "./Pages/Registration";
+import Home from "./Pages/Home";
+import Buyprods from "./Pages/Buyprods";
+import Merchant from "./Pages/Mechstore";
+import Store from "./Pages/Merchstore Pages/Store";
+import Cart from "./Pages/Cart";
+import Addproduct from "./Pages/Addproduct";
+import Products from "./Pages/Merchstore Pages/Products";
+import Prodinfo from "./Pages/Prodinfo";
+import Purchases from "./Pages/Purchases";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Navbar";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {location.pathname != "/" && location.pathname != "/registration" && (
+        <NavBar />
+      )}
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/buyprods" element={<Buyprods />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/prodinfo" element={<Prodinfo />} />
+        <Route path="/merchant/" element={<Merchant />}>
+          <Route path="store" element={<Store />} />
+          <Route path="products" element={<Products />} />
+        </Route>
+        <Route path="/addproducts" element={<Addproduct />} />
+        <Route path="/purchases" element={<Purchases />} />
+      </Routes>
     </div>
   );
 }
